617e2b8f0099acc7ec7a8244b12839125297d46d
LuisArmandoOlmos <luisarmandoolmosolmos@gmail.com>
2022-11-12 14:52:31 -0400

    Added project structure

---
+++ src/GifExpressApp.jsx
@@ -0,0 +1,7 @@
+export const GifExpressApp = () => {
+  return (
+    <div className="bg-black w-full h-screen p-6">
+      <h1 className="text-primary text-3xl font-extrabold">Gif Express App</h1>
+    </div>
+  );
+};
---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#F47521",
+      secondary: "#FF944D",
+      tertiary: "#FAB818",
+      black: "#000000",
+      white: "#FFFFFF",
+      gray: "#23252B",
+    },
+  },
+  plugins: [],
+};
